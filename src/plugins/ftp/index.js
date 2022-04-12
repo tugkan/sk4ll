f7e2d6ba54b5db6a72954213291f984b37256394
Tugkan Cengiz <tugkancengiz@gmail.com>
2022-04-12 22:33:07 +0300

    ftp module added

---
+++ src/plugins/ftp/index.js
@@ -0,0 +1,43 @@
+import util from 'util';
+import { exec } from 'child_process';
+import Logger from '@lib/logger';
+
+const execP = util.promisify(exec);
+
+const FTP = {
+    ports: [21],
+    name: 'FTP',
+    service: 'ftp',
+    init: async ({ ip, port }) => {
+        const output = await FTP.exec({ ip, port });
+        const isValid = FTP.validator({ ip, port, output });
+        if (isValid) {
+            await FTP.payload({ ip, port });
+        }
+    },
+    exec: async ({ ip, port }) => {
+        try {
+            Logger.debug(`Executing FTP with ip: ${ip} and port: ${port}`);
+            const { stdout } = await execP(`wget ftp://${ip}`);
+
+            return stdout;
+        } catch (error) {
+            console.log(error);
+            Logger.debug('Error executing FTP shell');
+        }
+        return '';
+    },
+    validator: ({ ip, port, output }) => {
+        if (output.includes('Logged in')) {
+            Logger.warn(`Found vulnerable FTP service on ${ip}:${port}`);
+            return true;
+        }
+
+        return false;
+    },
+    payload: ({ ip, port }) => {
+        Logger.warn(`PAYLOAD FTP: ${ip}:${port}`);
+    },
+};
+
+export default FTP;
